8265c40e58fbf7235e6842b11db20264fe94b70c
John <johnconley507@hotmail.com>
2016-07-05 14:45:38 -0400

    Uploading my new companies ;)

---
+++ timetracking/app/controllers/projects_controller.rb
@@ -0,0 +1,9 @@
+class ProjectsController < ApplicationController
+
+	def index
+		@projects_array = Project.order(created_at:"desc")
+									.limit(10)
+
+		render "index"
+	end
+end
--- timetracking/db/seeds.rb
+++ timetracking/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+projectA = Project.create(name: "Ironhack", description: "Coding Bootcamp")
+projectB = Project.create(name: "Alpha", description: "Coffee Company/Classified")
+projectC = Project.create(name: "Beta", description: "Systems Analysis Corp")
+projectD = Project.create(name: "Charlie", description: "SigInt Analysis Company") 
+projectE = Project.create(name: "BlackWater", description: "Military Contracting Company")
+projectF = Project.create(name: "Synchtronic", description: "Information Retrieval Service")
+projectG = Project.create(name: "Strange Sights", description: "Avant Garde Travel Company")
+projectH = Project.create(name: "Recoverable.Inc", description: "UFO Retrieval Service")
+projectI = Project.create(name: "MK Ultra", description: "Information Restricted")
+projectJ = Project.create(name: "Singularity", description: "Sub-Branch of NASA")
+
+puts "done"
