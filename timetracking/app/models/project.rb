ff13d22ba165bfe073f7c6efe8df076beeb023ee
John <johnconley507@hotmail.com>
2016-07-07 10:53:03 -0400

    Made a destroy

--- timetracking/app/models/project.rb
+++ timetracking/app/models/project.rb
@@ -1,11 +1,12 @@
  class Project < ApplicationRecord
  	has_many :time_entries
- 	validates :name, presence: true
- 	validates :email, presence: true, uniqueness: true
- 	validates :attribute, length: { in: 1..30	 }
- 	validates :attribute, format: { with: /[a-zA-Z0..9]\s}
+ 
+ 	def self.clean_old
+ 		old_project = Project.where('created_at < ?', Time.now - 2.days)
+ 		old_project.destroy_all
+ 	end
 end
 
 
-# the_project = Project.find_by(id: 1)
-# the_project.time_entries
+
+
